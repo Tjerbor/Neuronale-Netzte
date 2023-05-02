63c32b9767f97a0809bc805876263602fdd1bf7f
Piet Maier <pietmaier.github@protonmail.com>
2023-05-02 16:02:28 +0200

    Fix File Reader

--- src/utils/read_utils.java
+++ src/utils/read_utils.java
@@ -7,7 +7,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
@@ -35,6 +34,7 @@ public class read_utils {
 
     /**
      * creates the model.
+     *
      * @param path filepath with weights
      * @return Strucktur Layer
      * @throws IOException
@@ -48,43 +48,31 @@ public class read_utils {
 
         int[] topologie = IntStream.range(1, list.get(0).length).map(i -> Integer.parseInt(list.get(0)[i])).toArray();
 
-        System.out.println(Arrays.toString(topologie));
-        Layer[] structur = new Layer[((topologie.length-1)*2) +1];
-        structur[0] = Utils.getActivation();
+        Layer[] structur = new Layer[(topologie.length - 1) * 2];
 
         int count = 0;
         int list_pos = 1; //da die erste Zeile die Topologie bestimmt.
-        for (int i = 0; i < (topologie.length -1); i++) {
 
-            //double[][] w = new double[topologie[count]+1][topologie[count+1]];
-            double[][] w = Utils.genRandomWeights(topologie[i]+1, topologie[i+1]);
+        for (int i = 0; i < (topologie.length - 1); i++) {
+            double[][] w = Utils.genRandomWeights(topologie[i] + 1, topologie[i + 1]);
 
-            //starts with 1 becuause.
-            structur[1+count] = new FullyConnectedLayer(topologie[i], topologie[i+1]);
-            structur[1+count+1] = Utils.getActivation();
+            structur[count] = new FullyConnectedLayer(topologie[i], topologie[i + 1]);
+            structur[count + 1] = Utils.getActivation();
 
-
-            for (int j=0; j < (topologie[i])+1; j++){
-
-                System.out.println(list_pos);
-                //System.out.println(Arrays.toString(list.get(list_pos)));
-                for(int wi=0; wi < list.get(list_pos).length ;wi++) {
+            for (int j = 0; j < (topologie[i]) + 1; j++) {
+                for (int wi = 0; wi < list.get(list_pos).length; wi++) {
                     w[j][wi] = Double.parseDouble(list.get(list_pos)[wi]);
                 }
 
                 list_pos += 1; //update layer position.
             }
-            list_pos += 1; //wegen der leerzeile.
-            structur[1+i+count].weights = Utils.split_for_weights(w);
-            structur[1+i+count].biases = Utils.split_for_biases(w);
 
+            list_pos += 1; //wegen der leerzeile.
+            structur[i + count].weights = Utils.split_for_weights(w);
+            structur[i + count].biases = Utils.split_for_biases(w);
             count += 2;
-            list_pos += 1; //wegen der leerzeile.
-
         }
 
         return structur;
-
-
-
-    }}
+    }
+}
