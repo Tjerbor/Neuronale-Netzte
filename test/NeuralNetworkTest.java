c55d2df6b346af7c0ce34c3a8e7c18a462833e89
Piet Maier <pietmaier.github@protonmail.com>
2023-05-03 14:34:40 +0200

    Add Logical Conjunction Test, Add tearDown()

--- test/NeuralNetworkTest.java
+++ test/NeuralNetworkTest.java
@@ -1,14 +1,18 @@
 import layers.CustomActivation;
 import layers.Layer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import utils.Reader;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
+ * This class contains various tests to assert the correctness of the neural network.
+ *
  * @see <a href="https://junit.org/junit5/docs/current/user-guide/">JUnit Jupiter</a>
  */
 public class NeuralNetworkTest {
@@ -19,24 +23,42 @@ public class NeuralNetworkTest {
         neuralNetwork = new NeuralNetwork();
     }
 
+    @AfterEach
+    void tearDown() {
+        neuralNetwork = null;
+    }
+
+    @Test
+    void logicalConjunction() throws Exception {
+        neuralNetwork.create(Reader.create("csv/topology/logicalConjunction.csv"));
+
+        // TODO: Disable Bias Neuron & Set Activation Function
+
+        double[][] result = neuralNetwork.computeAll(new double[][]{{0, 0}, {0, 1}, {1, 0}, {1, 1}});
+
+        assertAll(
+                () -> assertEquals(0, result[0][0]),
+                () -> assertEquals(0, result[1][0]),
+                () -> assertEquals(0, result[2][0]),
+                () -> assertEquals(1, result[3][0])
+        );
+    }
+
     @Test
     void trafficLight() throws IOException {
-        Layer[] l2 = Reader.create("csv/topology/trafficLight.csv");
-
-
         neuralNetwork.create(Reader.create("csv/topology/trafficLight.csv"));
 
-        Layer[] l = neuralNetwork.getModel();
-        Layer cF = new CustomActivation(new String[]{"logi", "logi", "logi", "id"});
+        Layer[] topology = neuralNetwork.getModel();
 
-
-        l[1] = cF;
+        topology[1] = new CustomActivation(new String[]{"logi", "logi", "logi", "id"});
 
         double[] result = neuralNetwork.compute(new double[]{1, 0, 0});
 
-        assertEquals(-0.03504739174185117, result[0]);
-        assertEquals(0.06379840926271282, result[1]);
-        assertEquals(0.11976621345436994, result[2]);
-        assertEquals(0.032371141308330784, result[3]);
+        assertAll(
+                () -> assertEquals(-0.03504739174185117, result[0]),
+                () -> assertEquals(0.06379840926271282, result[1]),
+                () -> assertEquals(0.11976621345436994, result[2]),
+                () -> assertEquals(0.032371141308330784, result[3])
+        );
     }
 }
