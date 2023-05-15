5dcfcd2e22ecdf757849f9d99937805ee15e5b0a
guiliano99 <guilianolehmann@live.de>
2023-05-15 12:38:27 +0200

    added Sgn

---
+++ src/layer/Sgn.java
@@ -0,0 +1,19 @@
+package layer;
+
+
+/**
+ * This class models the signed disambiguation. function.
+ */
+public class Sgn extends Activation {
+
+    @Override
+    public double definition(double x) {
+        return x > 0 ? 1 : -1;
+    }
+
+    @Override
+    public double derivative(double x) {
+        return 0;
+    }
+
+}
