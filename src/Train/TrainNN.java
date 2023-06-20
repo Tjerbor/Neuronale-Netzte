38dabaa3dcd76ea10131b97b7b8252bb539130c8
dblade <guilianolehmann@live.de>
2023-06-20 16:17:25 +0200

    added Train Methode with API

---
+++ src/Train/TrainNN.java
@@ -0,0 +1,37 @@
+package Train;
+
+import layer.MSE;
+import layer.TanH;
+import main.MNIST;
+import main.NeuralNetwork;
+
+public class TrainNN {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        NeuralNetwork nn = new NeuralNetwork();
+
+        double[][][] trainingData = MNIST.read("data/mnist/train-images-idx3-ubyte.gz", "data/mnist/train-labels-idx1-ubyte.gz");
+        double[][] x_train = trainingData[0];
+        double[][] y_train = trainingData[1];
+
+
+        double[][][] testData = MNIST.read("data/mnist/t10k-images-idx3-ubyte.gz", "data/mnist/t10k-labels-idx1-ubyte.gz");
+        double[][] x_test = testData[0];
+        double[][] y_test = testData[1];
+
+        int epochs = 20;
+        double learning = 1e-4;
+
+
+        nn.create(new int[]{784, 49, 20, 10}, new TanH());
+        nn.setLoss(new MSE());
+        nn.train_single(epochs, x_train, y_train, learning);
+
+
+    }
+
+
+}
