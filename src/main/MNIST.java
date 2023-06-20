cf69771362f974598872915143d45289d9245309
Piet Maier <pietmaier.github@protonmail.com>
2023-06-20 17:35:50 +0200

    Add EMNIST Files

--- src/main/MNIST.java
+++ src/main/MNIST.java
@@ -8,6 +8,7 @@ import java.util.zip.GZIPInputStream;
 
 /**
  * This class contains utility methods for reading the <a href="http://yann.lecun.com/exdb/mnist/">MNIST database of handwritten digits</a>.
+ * It can also be used to read the <a href="https://www.nist.gov/itl/products-and-services/emnist-dataset">EMNIST database</a>.
  */
 public final class MNIST {
     private MNIST() {
@@ -42,15 +43,23 @@ public final class MNIST {
     }
 
     /**
-     * This method attempts to read the given MNIST data sets.
-     * It throws an exception if the file does not exist, an I/O error occurs, or the format is not correct.
-     * The return value is a three-dimensional array of length two containing the image and label data.
+     * This method attempts to read the given EMNIST data sets.
+     * The number of classes must be passed as an additional parameter.
      *
-     * @param idx3 Image Data
-     * @param idx1 Label Data
-     * @see <a href="http://yann.lecun.com/exdb/mnist/">MNIST database of handwritten digits</a>
+     * <ul>
+     *     <li>ByClass: 814,255 Characters, 62 Unbalanced Classes</li>
+     *     <li>ByMerge: 814,255 Characters, 47 Unbalanced Classes</li>
+     *     <li>Balanced: 131,600 Characters, 47 Balanced Classes</li>
+     *     <li>Letters: 145,600 Characters, 26 Balanced Classes</li>
+     *     <li>Digits: 280,000 Characters, 10 Balanced Classes</li>
+     *     <li>MNIST: 70,000 Characters, 10 Balanced Classes</li>
+     * </ul>
+     *
+     * @see <a href="https://www.itl.nist.gov/iaui/vip/cs_links/EMNIST/Readme.txt">README</a>
+     * @see <a href="https://arxiv.org/pdf/1702.05373v1.pdf">EMNIST Paper</a>
+     * @see MNIST#read(String, String)
      */
-    public static double[][][] read(String idx3, String idx1) throws IOException {
+    public static double[][][] read(String idx3, String idx1, int number) throws IOException {
         try (
                 InputStream imagesIn = new GZIPInputStream(new FileInputStream(idx3));
                 InputStream labelsIn = new GZIPInputStream(new FileInputStream(idx1))
@@ -77,7 +86,7 @@ public final class MNIST {
 
             double[][] images = new double[length][PIXELS];
 
-            double[][] labels = new double[length][DIGITS];
+            double[][] labels = new double[length][number];
 
             for (int i = 0; i < length; i++) {
                 for (int j = 0; j < PIXELS; j++) {
@@ -99,6 +108,19 @@ public final class MNIST {
 
             return new double[][][]{images, labels};
         }
+    }
+
+    /**
+     * This method attempts to read the given MNIST data sets.
+     * It throws an exception if the file does not exist, an I/O error occurs, or the format is not correct.
+     * The return value is a three-dimensional array of length two containing the image and label data.
+     *
+     * @param idx3 Image Data
+     * @param idx1 Label Data
+     * @see MNIST#read(String, String, int)
+     */
+    public static double[][][] read(String idx3, String idx1) throws IOException {
+        return read(idx3, idx1, DIGITS);
     }
 
     public static double[][][] x_train_2_batch(double[][] x_train, int batch_size) throws Exception {
